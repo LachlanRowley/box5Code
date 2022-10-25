fdbcfdbfe52421daedfe1d813dd239f54c576a80
LachlanRowley <62129160+LachlanRowley@users.noreply.github.com>
2022-10-25 14:44:45 +1100

    Replaced secondary pattern switching with full colour

--- main/main.ino
+++ main/main.ino
@@ -5,12 +5,11 @@
 // Initialise Servo Variables
 const int DEFAULT_SERVO_POS = 0;
 const int SERVO_END_POS = 90;
-const int NEO_PIXEL_COUNT = 24;
+const int NEO_PIXEL_COUNT = 20;
 const int SENSITIVITY = 950;
-
-
 const int PATTERNCOUNT = 3;
-// Define Pattern Class
+
+// Define Sequential Pattern Class
 class SequentialPattern {
 	protected:
 		Adafruit_NeoPixel light;
@@ -23,17 +22,18 @@ class SequentialPattern {
 			light = l;
 		}
 
-	bool isActive = false;
-	void process() {
-		if(isActive) {
-			if(millis() > endTime) {
-	   	        patternInc();
-				endTime = millis()+timeDelay;
-			}
-		}
-    }
+        bool isActive = false;
+        void process() {
+            if(isActive) {
+                if(millis() > endTime) {
+                    patternInc();
+                    endTime = millis()+timeDelay;
+                }
+            }
+        }
   
 	void patternStart() {
+        light.clear();
 		light.setPixelColor(curOnLight, light.Color(random(0,255), random(0,255), random(0,255)));
 		light.show();
 		isActive = true;
@@ -46,7 +46,7 @@ class SequentialPattern {
 		light.clear();    
 		light.setPixelColor(curOnLight, light.Color(random(0,255), random(0,255), random(0,255)));
 		light.show();
-	}
+    }
     void patternEnd() {
 		light.clear();
       	curOnLight = 0;
@@ -55,98 +55,161 @@ class SequentialPattern {
 
 // Define ColumnsPattern Class
 class ColumnsPattern {
-protected:
-    Adafruit_NeoPixel light;
-    int curOnLight = 0;
-    long timeDelay = 100;
-    long endTime;
+  protected:
+      Adafruit_NeoPixel light;
+      int curOnLight = 0;
+      long timeDelay = 100;
+      long endTime;
 
-public:
-    ColumnsPattern() {}
-    ColumnsPattern(Adafruit_NeoPixel l) {
-        light = l;
-    }
-    bool isActive = false;
-    void process() {
-        if (isActive){
-            if (millis() > endTime){
-                patternInc();
-                endTime = millis() + timeDelay;
-            }
-        }
-    }
+  public:
+      ColumnsPattern() {}
+      ColumnsPattern(Adafruit_NeoPixel l) {
+          light = l;
+      }
+      bool isActive = false;
+      void process() {
+          if (isActive){
+              if (millis() > endTime){
+                  patternInc();
+                  endTime = millis() + timeDelay;
+              }
+          }
+      }
 
-    void patternStart() {
-        light.setPixelColor(curOnLight, (255, 0, 0));
-        light.show();
-        isActive = true;
-    }
+      void patternStart() {
+          light.clear();
+          light.setPixelColor(curOnLight, (255, 0, 0));
+          light.show();
+          isActive = true;
+          endTime = millis() + timeDelay;
+      }
 
-    void patternInc() {
-        light.clear();
+      void patternInc() {
+          light.clear();
 
-        if (curOnLight == 6) {
-            curOnLight = 0;
-        }
-        curOnLight = curOnLight % NEO_PIXEL_COUNT;
-        light.setPixelColor(curOnLight, light.Color(255, 0, 0));
-        light.setPixelColor(curOnLight + 6 % 24, light.Color(255, 0, 0));
-        light.setPixelColor(curOnLight + 12 % 24, light.Color(255, 0, 0));
-        light.setPixelColor(curOnLight + 18 % 24, light.Color(255, 0, 0));
-        light.show();
-        curOnLight++;
-    }
-    void patternEnd() {
-        light.clear();
-      	curOnLight = 0;
-    }
+          if (curOnLight == 6) {
+              curOnLight = 0;
+          }
+          curOnLight = curOnLight % NEO_PIXEL_COUNT;
+          light.setPixelColor(curOnLight, light.Color(255, 0, 0));
+          light.setPixelColor(curOnLight + 6 % 24, light.Color(255, 0, 0));
+          light.setPixelColor(curOnLight + 12 % 24, light.Color(255, 0, 0));
+          light.setPixelColor(curOnLight + 18 % 24, light.Color(255, 0, 0));
+          light.show();
+          curOnLight++;
+      }
+      void patternEnd() {
+          light.clear();
+          curOnLight = 0;
+      }
 };
 
-//Loading Pattern
+// Define Row Pattern Class
+class RowPattern {
+  protected:
+      Adafruit_NeoPixel light;
+      int curOnLight = 0;
+      long timeDelay = 100;
+      long endTime;
+
+  public:
+      RowPattern() {}
+      RowPattern(Adafruit_NeoPixel l) {
+          light = l;
+      }
+      bool isActive = false;
+      void process() {
+          if (isActive) {
+              if (millis() > endTime) {
+                  patternInc();
+                  endTime = millis() + timeDelay;
+              }
+          }
+      }
+
+  void patternStart() {
+    light.clear();
+  	light.setPixelColor(curOnLight, (255,0,0));
+    light.show();
+    isActive = true;
+    endTime = millis() + timeDelay;
+  }
+  
+  void patternInc() {
+    light.clear(); 
+    
+    if (curOnLight == NEO_PIXEL_COUNT){
+      curOnLight = 0;
+    }
+   	
+    curOnLight = curOnLight % NEO_PIXEL_COUNT;   
+    light.setPixelColor(curOnLight, light.Color(255,0,0));
+    light.setPixelColor(curOnLight + 1 % 24, light.Color(255,0,0));
+    light.setPixelColor(curOnLight + 2 % 24, light.Color(255,0,0));
+    light.setPixelColor(curOnLight + 3 % 24, light.Color(255,0,0));
+    light.setPixelColor(curOnLight + 4 % 24, light.Color(255,0,0));
+    light.setPixelColor(curOnLight + 5 % 24, light.Color(255,0,0));
+    light.show();
+    curOnLight += 6;
+  }
+  
+  void patternEnd() {
+	light.clear();
+    curOnLight = 0;
+  }
+  
+};
+
+// Define Loading Pattern Class
 class LoadingPattern {
-protected:
-    Adafruit_NeoPixel light;
-    int curOnLight = 0;
-    long timeDelay = 100;
-    long endTime;
+  protected:
+      Adafruit_NeoPixel light;
+      int curOnLight = 0;
+      long timeDelay = 100;
+      long endTime;
 
-public:
-    LoadingPattern() {}
-    LoadingPattern(Adafruit_NeoPixel l) {
-        light = l;
-    }
-    bool isActive = false;
-    void process() {
-        if (isActive) {
-            if (millis() > endTime) {
-                patternInc();
-                endTime = millis() + timeDelay;
-            }
+  public:
+      LoadingPattern() {}
+      LoadingPattern(Adafruit_NeoPixel l) {
+          light = l;
+      }
+      bool isActive = false;
+      void process() {
+          if (isActive) {
+              if (millis() > endTime) {
+                  patternInc();
+                  endTime = millis() + timeDelay;
+              }
+          }
+      }
+
+      void patternStart() {
+          light.clear();
+          light.setPixelColor(curOnLight, (255, 0, 0));
+          light.show();
+          isActive = true;
+          endTime = millis() + timeDelay;
+      }
+
+      void patternInc() {
+        if(curOnLight == NEO_PIXEL_COUNT + 1) {
+          curOnLight=0;
+          light.clear();
         }
-    }
-
-    void patternStart() {
-        light.setPixelColor(curOnLight, (255, 0, 0));
-        light.show();
-        isActive = true;
-    }
-
-    void patternInc() {
-        curOnLight = curOnLight % NEO_PIXEL_COUNT;
-
         light.setPixelColor(curOnLight, light.Color(255, 0, 0));
         light.show();
         curOnLight++;
-    }
-    void patternEnd() {
-		light.clear();
-      	curOnLight = 0;
-    }
+      }
+  
+      void patternEnd() {
+          light.clear();
+          curOnLight = 0;
+      }
 };
 
 // Define Box Side Class
 class BoxSide {
-    bool isActive = false;
+  bool isActive = false;
 
     // Initialise Timers
 	long servoTimerEndTime;
@@ -155,20 +218,20 @@ class BoxSide {
 	bool servoTimerStarted;
     
 	int lightPin;
-	Adafruit_NeoPixel light;
 
 	Servo servo;
 	byte sensorPin;
-
   
-	int activePatternNum = -1;
 
 	//Patterns
 	SequentialPattern sqPat;
   	ColumnsPattern coPat;
   	LoadingPattern ldPat;
+  	RowPattern rwPat;
 
 	public:
+	  	Adafruit_NeoPixel light;
+		int activePatternNum = -3;
         BoxSide *neighbourSide;
 		BoxSide(Servo sv, int l, byte sen) {
             servo = sv;
@@ -186,7 +249,7 @@ class BoxSide {
 		processPattern();
       	//If the sensor has not been triggered read sensor value
 		if (!isActive) {
-			if(analogRead(sensorPin) < SENSITIVITY) {
+			if(digitalRead(sensorPin) == HIGH) {
 				onSensorTriggered();
 			}
 			return;
@@ -201,9 +264,9 @@ class BoxSide {
 	// Method to define what happens when sensor is triggered
 	void onSensorTriggered() {
 		isActive = true;
-		startTimer();
+		startTimer();   
       	patternStart(-1);
-      	neighbourSide->patternStart(-2);
+      	neighbourSide->patternExit();
 	}
 
 	// Method to define what happens when the timer starts
@@ -237,6 +300,7 @@ class BoxSide {
 		sqPat = SequentialPattern(light);
       	coPat = ColumnsPattern(light);
       	ldPat = LoadingPattern(light);
+      	rwPat = RowPattern(light);
 	}
 	
 	//Call setup function (if exists) for pattern
@@ -248,57 +312,74 @@ class BoxSide {
 	    if(genRnd == -1) {
 			patId = random(PATTERNCOUNT);
     	}
-      	patternExit();
 		switch(patId) {
-          	case -2:
-          		ldPat.patternStart();
-          		break;
 	        case 0:
 				sqPat.patternStart();
 		        break;
           	case 1:
           		coPat.patternStart();
           		break;
-		}
-		activePatternNum = patId;
+          	case 2:
+          		rwPat.patternStart();
+          		break;
+          	case 3:
+          		ldPat.patternStart();
+          		break;
+
+        }
+		this->activePatternNum = patId;
 	}
 	
 	void processPattern() {
-        switch(activePatternNum) {
-            case -2:
-          		ldPat.process();
-          		break;
+		switch(this->activePatternNum) {
 			case 0:
 				sqPat.process();
-       	        break;
+          		break;
             case 1:
-                coPat.process();
-                break;
+          		coPat.process();
+          		break;
+			case 2:
+          		rwPat.process();
+          		break;
+            case 3:
+  		        ldPat.process();
+        		break;
+
         }
-  }
+	}
 
   	void patternExit() {
-		switch(activePatternNum) {
-          	case -2:
-          		ldPat.patternEnd();
-          		break;
-	          case 0:
+        this->activePatternNum = -3;
+      	light.fill(light.Color(255,0,255));
+      	light.show();
+        return;
+
+		switch(this->activePatternNum) {
+	        case 0:
 				sqPat.patternEnd();
           		break;
             case 1:
           		coPat.patternEnd();
           		break;
+          	case 2:
+          		rwPat.patternEnd();
+          		break;
+          	case 3:
+          		ldPat.patternEnd();
+          		break;
+
         }
-    }
+        Serial.println("Exitted");
+	}
 };
 
-const byte sensorPinLeft = A0;
-const byte sensorPinRight = -1;
+const byte sensorPinLeft = 2;
+const byte sensorPinRight = 3;
 
-int lightPinsLeft = 2;
-int lightPinsRight = -1;
+int lightPinsLeft = 7;
+int lightPinsRight = 6;
 
-int servoPinLeft = 10;
+int servoPinLeft = -1;
 int servoPinRight = -1;
 
 Servo leftServo;
@@ -310,9 +391,9 @@ BoxSide rightSide(rightServo, lightPinsRight, sensorPinRight);
 
 // Define Setup Function
 void setup(){
-	Serial.begin(9600);
-	pinMode(lightPinsLeft, OUTPUT);
-	pinMode(lightPinsRight, OUTPUT);
+  	randomSeed(analogRead(8));
+	//pinMode(lightPinsLeft, OUTPUT);
+	//pinMode(lightPinsRight, OUTPUT);
 
 	leftServo.write(DEFAULT_SERVO_POS);
 	rightServo.write(DEFAULT_SERVO_POS);
@@ -322,8 +403,8 @@ void setup(){
 	pinMode(sensorPinLeft, INPUT);
 	pinMode(sensorPinRight, INPUT);
   
-	leftSide.neighbourSide = &rightSide;
-	rightSide.neighbourSide = &leftSide;   
+  	leftSide.neighbourSide = &rightSide;
+  	rightSide.neighbourSide = &leftSide;
 }
 
 // Define Loop Function
